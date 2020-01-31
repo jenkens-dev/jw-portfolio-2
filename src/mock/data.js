2eaafa6a144c89bf17ee49f17964cd040e9504cf
Taljjaa <jenken131@gmail.com>
2020-01-31 13:57:12 -0800

    added pictures to project section, described technology used in projects and updated abot me section

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,31 +18,31 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: `Hi my name is Jennifer Williams and I'm a full stack developer.`,
+  paragraphTwo: `I have a background in Molecular Biology and a degree in Zoology. I taught myself programming for 2 years before deciding to join Flatiron School. I love building all aspects of an application but specialize in the frontend.`,
+  paragraphThree: 'In my free time I enjoy reading, watching anime, and playing with my dog!',
+  resume: 'https://drive.google.com/open?id=1k_uEP3y7ENRrp059jqDGofiu2OHwFlY2', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'BookclubBuilder.png',
     title: 'Bookclub Builder',
     info: `Bookclub Builder's goal is to encourage people to read more and unite book lovers. I wanted a simple website that allowed me to create a book club and decide on which books to read through poll.`,
-    info2:
-      'Users can sign up and login. Users can join a book club or leave a book club and as a member of the book club a user can vote on the current poll. Users can create a book club. If you create the book club you are automatically set as the admin. As the admin you can create and archive polls and edit the book club',
+    info2: 'Built with React, Redux, MaterialUI, on the frontend and Ruby on Rails on the backend',
     url: 'https://bookclub-builder-client.herokuapp.com/',
     repo: 'https://github.com/Taljjaa/bookclub-builder-frontend', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'KrazyPoems.png',
     title: 'Krazy Poems',
     info:
       'Krazy Poems is a program designed to liven up poems or text by identifying nouns, adjectives, verbs, and adverbs in a sentence and replacing them with other random words. You can also have your new krazy poem read aloud by a diverse set of different voices and even add music to the reading!',
-    info2: '',
+    info2:
+      'Single Page Application built with JavaScript on the frontend and Ruby on Rails on the backend',
     url: '',
     repo: 'https://github.com/Taljjaa/noun-swapper-frontend', // if no repo, the button will not show up
   },

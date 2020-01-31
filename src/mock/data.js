b679bb2b2588d1a185c1cffb82cdcbddbe80c598
Taljjaa <jenken131@gmail.com>
2020-01-31 11:36:12 -0800

    updated heroData and added bookclub builder to project data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Jennifer Williams | Full Stack Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Jennifer Williams',
+  subtitle: 'I am a Full Stack Developer',
   cta: '',
 };
 
@@ -29,11 +29,12 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Bookclub Builder',
+    info: `Bookclub Builder's goal is to encourage people to read more and unite book lovers. I wanted a simple website that allowed me to create a book club and decide on which books to read through poll.`,
+    info2:
+      'Users can sign up and login. Users can join a book club or leave a book club and as a member of the book club a user can vote on the current poll. Users can create a book club. If you create the book club you are automatically set as the admin. As the admin you can create and archive polls and edit the book club',
+    url: 'https://bookclub-builder-client.herokuapp.com/',
+    repo: 'https://github.com/Taljjaa/bookclub-builder-frontend', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
@@ -90,5 +91,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
